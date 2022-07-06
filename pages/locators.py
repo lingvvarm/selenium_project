99bf919224716c235f221a83ec1d802651e9ea6f
Andreiev Danylo <lingvvarm@MacBook-Pro-Andreiev.local>
2022-07-06 19:27:43 +0300

    Added new methods in BasePage class

--- pages/locators.py
+++ pages/locators.py
@@ -22,3 +22,7 @@ class ProductPageLocators():
     MESSAGE_ABOUT_ADDING = (By.CSS_SELECTOR, '.alertinner')
     BASKET_PRICE = (By.CSS_SELECTOR, '.alertinner p strong')
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -14,6 +14,7 @@ def test_guest_can_add_product_to_basket(browser, number):
     page.open()
     page.add_product_to_basket()
 
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -27,6 +28,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/uk/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -34,5 +36,15 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_product_to_basket()
     page.should_not_be_success_message()
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
